65f8e9a90af2429580430c693edf643779ceebd5
Erin Woody <erinelizabethwoody@gmail.com>
2017-08-01 16:11:59 -0500

    added css

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ for (i = 0; i < customers.results.length; i++) {
             <img src=${customers.results[i].picture.large} alt="pic1">
             <p class="fullName">${customers.results[i].name.first} ${customers.results[i].name.last}</p>
             <p class="email">${customers.results[i].email}</p>
-            <p class="address">${customers.results[i].location.street} <br> ${customers.results[i].location.city}, ${customers.results[i].location.state} ${customers.results[i].location.postcode}  <br> (518)-664-5821 </p>
-            <p class="ssn">544-97-7715</p>
+            <p class="address">${customers.results[i].location.street}</p>
+            <p class="address">${customers.results[i].location.city}, ${customers.results[i].location.state} ${customers.results[i].location.postcode} </p>
+            <p class="address">${customers.results[i].phone}</p>
+            <p class="ssn">${customers.results[i].id.value}</p>
         </div>
     `;
     document.querySelector('.grid').innerHTML += content;
